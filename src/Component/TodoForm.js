6343eb713aaf11730caa4aa3cf81f7a64b0b445c
Baskanbetul <btlbskn@hotmail.com>
2022-11-07 16:25:31 -0700

    Set up todo Form

--- src/Component/TodoForm.js
+++ src/Component/TodoForm.js
@@ -6,10 +6,19 @@ const TodoForm = () => {
     task: "",
     completed:false
   })
+
+  const handleTaskInputChange = (e) => {
+    setTodo({ ...todo, task:e.target.value });
+  }
   return (
     <form>
-      <input />
-      <button /> 
+      <input 
+      name = "task"
+      type = "text"
+      value = {todo.task}
+      onChange={handleTaskInputChange} 
+      />
+      <button type='submit'/> 
 
     </form>
   )
